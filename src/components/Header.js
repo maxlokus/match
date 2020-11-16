4431cb33b7113b31e46586f2385f1d8996259182
Max PM61Data <macklock@yandex.ru>
2020-11-16 21:32:01 +0300

    Login folder created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 
+import "./style.less";
+
 const { Header } = Layout;
 
 export const PageHeader = () => {
-  return (
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Home</Menu.Item>
-        <Menu.Item key="2">Notifications</Menu.Item>
-      </Menu>
-    </Header>
-  )
-}
+    return (
+        <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+                <Menu.Item key="1">Home</Menu.Item>
+                <Menu.Item key="2">Notifications</Menu.Item>
+            </Menu>
+        </Header>
+    );
+};
